fe48de07cd7d39dd54bc5bf120eb160d2c629ce4
724789975 <724789975@qq.com>
2021-02-19 00:13:04 +0800

    添加编辑器
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ finite_state_machine/fsm_editor/fsm_editor.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace fsm_editor
+{
+    partial class fsm_editor
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add_enum = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // add_enum
+            // 
+            this.add_enum.Location = new System.Drawing.Point(13, 13);
+            this.add_enum.Name = "add_enum";
+            this.add_enum.Size = new System.Drawing.Size(75, 23);
+            this.add_enum.TabIndex = 0;
+            this.add_enum.Text = "button1";
+            this.add_enum.UseVisualStyleBackColor = true;
+            this.add_enum.Click += new System.EventHandler(this.add_enum_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkedListBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 119);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(449, 250);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.checkedListBox1.Location = new System.Drawing.Point(7, 53);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 84);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // fsm_editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.add_enum);
+            this.Name = "fsm_editor";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.fsm_editor_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add_enum;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ finite_state_machine/fsm_editor/fsm_editor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fsm_editor
+{
+    public partial class fsm_editor : Form
+    {
+        public fsm_editor()
+        {
+            InitializeComponent();
+        }
+
+
+        private void fsm_editor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_enum_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
